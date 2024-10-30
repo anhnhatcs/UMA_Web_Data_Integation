72dbbeb11c2b6117efc3ac6edb785fc8d50c4b15
Erin <esperanzacheng1117@gmail.com>
2024-10-30 15:46:57 +0100

    Add models and string-based metrics: Jaccard, Equal, Levenshtein

---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/IR_using_linear_combination_a_b.java
@@ -0,0 +1,113 @@
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution;
+import java.io.File;
+import org.slf4j.Logger;
+
+import de.uni_mannheim.informatik.dws.winter.matching.MatchingEngine;
+import de.uni_mannheim.informatik.dws.winter.matching.MatchingEvaluator;
+import de.uni_mannheim.informatik.dws.winter.matching.blockers.NoBlocker;
+import de.uni_mannheim.informatik.dws.winter.matching.rules.LinearCombinationMatchingRule;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.HashedDataSet;
+import de.uni_mannheim.informatik.dws.winter.model.MatchingGoldStandard;
+import de.uni_mannheim.informatik.dws.winter.model.Performance;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.model.io.CSVCorrespondenceFormatter;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+import de.uni_mannheim.informatik.dws.winter.utils.WinterLogManager;
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Song;
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.SongXMLReader;
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Comparators.SongComparatorJaccard;
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Comparators.SongTitleComparatorEqual;
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.Comparators.SongTitleComparatorLevenshtein;
+
+
+public class IR_using_linear_combination_a_b {
+	/*
+	 * Logging Options:
+	 * 		default: 	level INFO	- console
+	 * 		trace:		level TRACE     - console
+	 * 		infoFile:	level INFO	- console/file
+	 * 		traceFile:	level TRACE	- console/file
+	 *  
+	 * To set the log level to trace and write the log to winter.log and console, 
+	 * activate the "traceFile" logger as follows:
+	 *     private static final Logger logger = WinterLogManager.activateLogger("traceFile");
+	 *
+	 */
+
+	private static final Logger logger = WinterLogManager.activateLogger("default");
+	
+    public static void main( String[] args ) throws Exception
+    {
+		// loading data
+		logger.info("*\tLoading datasets\t*");
+		HashedDataSet<Song, Attribute> dataApple = new HashedDataSet<>();
+		new SongXMLReader().loadFromXML(new File("data/input/apple.xml"), "/songs/song", dataApple);
+		HashedDataSet<Song, Attribute> dataOpenDB = new HashedDataSet<>();
+		new SongXMLReader().loadFromXML(new File("data/input/opendb.xml"), "/songs/song", dataOpenDB);
+
+		// load the gold standard (test set)
+		logger.info("*\tLoading gold standard\t*");
+		MatchingGoldStandard gsTest = new MatchingGoldStandard();
+		gsTest.loadFromCSVFile(new File(
+				"data/goldstandard/gs_Song_apple.csv"));
+
+		// create a matching rule
+		// set the finalThreshold as our data requires
+		LinearCombinationMatchingRule<Song, Attribute> matchingRule = new LinearCombinationMatchingRule<>(
+				0);
+		matchingRule.activateDebugReport("data/output/debugResultsMatchingRule.csv", 1000, gsTest);
+		
+		// add comparators
+		// here defines the weight of matching attributes
+		matchingRule.addComparator(new SongTitleComparatorLevenshtein(), 0.4);
+		matchingRule.addComparator(new SongComparatorJaccard(), 0.3);
+		matchingRule.addComparator(new SongTitleComparatorEqual(), 0.3);
+	
+
+		// create a blocker (blocking strategy)
+		// StandardRecordBlocker<Song, Attribute> blocker = new StandardRecordBlocker<Song, Attribute>(new SongBlockingKeyByTitleGenerator());
+		NoBlocker<Song, Attribute> blocker = new NoBlocker<>();
+//		SortedNeighbourhoodBlocker<Movie, Attribute, Attribute> blocker = new SortedNeighbourhoodBlocker<>(new MovieBlockingKeyByTitleGenerator(), 1);
+		blocker.setMeasureBlockSizes(true);
+		//Write debug results to file:
+		blocker.collectBlockSizeData("data/output/debugResultsBlocking.csv", 100);
+		
+		// Initialize Matching Engine
+		MatchingEngine<Song, Attribute> engine = new MatchingEngine<>();
+
+		// Execute the matching
+		logger.info("*\tRunning identity resolution\t*");
+		Processable<Correspondence<Song, Attribute>> correspondences = engine.runIdentityResolution(
+				// this order should match the column order in the golden standard
+				dataApple, dataOpenDB, null, matchingRule,
+				blocker);
+
+		// Create a top-1 global matching
+		// Could unmark and run MaximumBipartiteMatchingAlgorithm if we are sure there are no duplicate in our dataset
+//		  correspondences = engine.getTopKInstanceCorrespondences(correspondences, 1, 0.0);
+
+//		 Alternative: Create a maximum-weight, bipartite matching
+//		 MaximumBipartiteMatchingAlgorithm<Movie,Attribute> maxWeight = new MaximumBipartiteMatchingAlgorithm<>(correspondences);
+//		 maxWeight.run();
+//		 correspondences = maxWeight.getResult();
+
+		// write the correspondences to the output file
+		new CSVCorrespondenceFormatter().writeCSV(new File("data/output/Song_apple_correspondences.csv"), correspondences);		
+		
+		logger.info("*\tEvaluating result\t*");
+		// evaluate your result
+		MatchingEvaluator<Song, Attribute> evaluator = new MatchingEvaluator<Song, Attribute>();
+		Performance perfTest = evaluator.evaluateMatching(correspondences,
+				gsTest);
+
+		// print the evaluation result
+		logger.info("Apple <-> OpenDB");
+		logger.info(String.format(
+				"Precision: %.4f",perfTest.getPrecision()));
+		logger.info(String.format(
+				"Recall: %.4f",	perfTest.getRecall()));
+		logger.info(String.format(
+				"F1: %.4f",perfTest.getF1()));
+    }
+}
---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/model/SongCSVFormatter.java
@@ -0,0 +1,27 @@
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model;
+
+import java.util.List;
+import de.uni_mannheim.informatik.dws.winter.model.DataSet;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.model.io.CSVDataSetFormatter;
+
+
+public class SongCSVFormatter extends CSVDataSetFormatter<Song, Attribute> {
+
+    @Override
+    public String[] getHeader(List<Attribute> orderedHeader) {
+        return new String[] { "id", "artist", "track", "trackExplicitness", "albumYear", "duration" };
+    }
+
+    @Override
+    public String[] format(Song record, DataSet<Song, Attribute> dataset, List<Attribute> orderedHeader) {
+        return new String[] {
+                record.getId(),
+                record.getArtist(),
+                record.getTrack(),
+                record.getTrackExplicitness(),
+                Integer.toString(record.getAlbumYear()),
+                Integer.toString(record.getDuration())
+        };
+    }
+}
