b487b206a842e9e8d4d3db6e09827b0874213b5f
anananna0v0 <joanna19990422@gmail.com>
2024-11-17 16:48:28 +0100

    Add fuser: Duration & Explicitness

---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/fusers/DurationFuserVoting.java
@@ -0,0 +1,39 @@
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.fusers;
+
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Song;
+import de.uni_mannheim.informatik.dws.winter.datafusion.AttributeValueFuser;
+import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.Voting;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.FusedValue;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.RecordGroup;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+
+/**
+ * {@link AttributeValueFuser} for the titles of {@link Song}s.
+ */
+public class DurationFuserVoting extends AttributeValueFuser<Integer, Song, Attribute> {
+
+	public DurationFuserVoting() {
+		super(new Voting<Integer, Song, Attribute>());
+	}
+	
+	@Override
+	public boolean hasValue(Song record, Correspondence<Attribute, Matchable> correspondence) {
+		return record.hasValue(Song.DURATION);
+	}
+	
+	@Override
+	public Integer getValue(Song record, Correspondence<Attribute, Matchable> correspondence) {
+		return record.getDuration();
+	}
+
+	@Override
+	public void fuse(RecordGroup<Song, Attribute> group, Song fusedRecord, Processable<Correspondence<Attribute, Matchable>> schemaCorrespondences, Attribute schemaElement) {
+		FusedValue<Integer, Song, Attribute> fused = getFusedValue(group, schemaCorrespondences, schemaElement);
+		fusedRecord.setDuration(fused.getValue());
+		fusedRecord.setAttributeProvenance(Song.DURATION, fused.getOriginalIds());
+	}
+
+}
---
+++ src/main/java/de/uni_mannheim/informatik/dws/wdi/ExerciseIdentityResolution/fusers/ExplicitnessFavourSource.java
@@ -0,0 +1,40 @@
+package de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.fusers;
+
+import de.uni_mannheim.informatik.dws.wdi.ExerciseIdentityResolution.model.Song;
+import de.uni_mannheim.informatik.dws.winter.datafusion.AttributeValueFuser;
+import de.uni_mannheim.informatik.dws.winter.datafusion.conflictresolution.meta.FavourSources;
+import de.uni_mannheim.informatik.dws.winter.model.Correspondence;
+import de.uni_mannheim.informatik.dws.winter.model.FusedValue;
+import de.uni_mannheim.informatik.dws.winter.model.Matchable;
+import de.uni_mannheim.informatik.dws.winter.model.RecordGroup;
+import de.uni_mannheim.informatik.dws.winter.model.defaultmodel.Attribute;
+import de.uni_mannheim.informatik.dws.winter.processing.Processable;
+
+/**
+ * {@link AttributeValueFuser} for the titles of {@link Song}s.
+ */
+public class ExplicitnessFavourSource extends AttributeValueFuser<String, Song, Attribute> {
+
+	public ExplicitnessFavourSource() {
+		super(new FavourSources<String, Song, Attribute>());
+	}
+
+	@Override
+	public boolean hasValue(Song record, Correspondence<Attribute, Matchable> correspondence) {
+		return record.hasValue(Song.TRACK_EXPLICITNESS);
+	}
+
+	@Override
+	public String getValue(Song record, Correspondence<Attribute, Matchable> correspondence) {
+		return record.getTrackExplicitness();
+	}
+
+	@Override
+	public void fuse(RecordGroup<Song, Attribute> group, Song fusedRecord, Processable<Correspondence<Attribute, Matchable>> schemaCorrespondences, Attribute schemaElement) {
+		FusedValue<String, Song, Attribute> fused = getFusedValue(group, schemaCorrespondences, schemaElement);
+		fusedRecord.setTrackExplicitness(fused.getValue());
+		fusedRecord.setAttributeProvenance(Song.TRACK_EXPLICITNESS,
+				fused.getOriginalIds());
+	}
+
+}
